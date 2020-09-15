bd1e9b2ec1bf5e9a669b8fd874433cf00aec5ff4
Hannes Braun <hannesbraun@mail.de>
2020-09-15 17:50:36 +0200

    magma-fat-proxy 1.0.1 revision 1
    Add the chmod command again for the start script

--- Formula/magma-fat-proxy.rb
+++ Formula/magma-fat-proxy.rb
@@ -4,6 +4,7 @@ class MagmaFatProxy < Formula
   url "https://github.com/magmaOffenburg/magmaFatProxy/archive/v1.0.1.tar.gz"
   sha256 "1dbdc1ae8732f41abafe44d56e3aa4a53575b7b8cb28844849948b62ae7a68e7"
   license "GPL-3.0"
+  revision 1
   head "https://github.com/magmaOffenburg/magmaFatProxy.git"
 
   depends_on "ant" => :build
@@ -14,6 +15,7 @@ class MagmaFatProxy < Formula
     libexec.install "jar/magmaFatProxy.jar"
     libexec.install "scripts/start.sh"
     mv libexec/"start.sh", libexec/"magma-fat-proxy"
+    chmod 0775, libexec/"magma-fat-proxy"
     bin.write_exec_script libexec/"magma-fat-proxy"
   end
 end
