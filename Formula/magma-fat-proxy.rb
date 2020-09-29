6dd2a09b2a2898ef2598a86c55a3a069590c8158
Hannes Braun <hannesbraun@mail.de>
2020-09-29 13:57:11 +0200

    Fix magma-fat-proxy

--- Formula/magma-fat-proxy.rb
+++ Formula/magma-fat-proxy.rb
@@ -4,6 +4,7 @@ class MagmaFatProxy < Formula
   url "https://github.com/magmaOffenburg/magmaFatProxy/archive/v1.1.0.tar.gz"
   sha256 "e785ba80ce3adfef85783dbbb9bdb97303324a3e8ccaa80462793dab70fce4ff"
   license "GPL-3.0"
+  revision 1
   head "https://github.com/magmaOffenburg/magmaFatProxy.git"
 
   depends_on "maven" => :build
@@ -12,6 +13,7 @@ class MagmaFatProxy < Formula
   def install
     system "mvn", "package"
     libexec.install "target/magmaFatProxy-#{version}-jar-with-dependencies.jar"
+    mv libexec/"target/magmaFatProxy-#{version}-jar-with-dependencies.jar", libexec/"magmaFatProxy.jar"
     libexec.install "scripts/start.sh"
     mv libexec/"start.sh", libexec/"magma-fat-proxy"
     chmod 0775, libexec/"magma-fat-proxy"
